5cbf4ee807155b789b7d7a52d210e9d76eefe6fd
Schischu <schischu65@gmail.com>
2014-06-01 22:29:09 +0200

    Fixup target chip

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,15 +20,13 @@
 # definition file).
 #
 
-# inherit from common msm8960
--include device/samsung/msm8960-common/BoardConfigCommon.mk
+# inherit from common msm8974
+-include device/samsung/msm8974-common/BoardConfigCommon.mk
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/lt03lte/include
 
 
-# overrides  msm8960
-TARGET_BOARD_PLATFORM := msm8974
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 
 # Kernel Configs
@@ -70,23 +68,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/lt03lte/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/lt03lte/bluetooth/vnd_lt03lte.txt
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
-# camera
-TARGET_PROVIDES_CAMERA_HAL_MSM8974 := true
-TARGET_PROVIDES_CAMERA_HAL :=
-
-# NFC
-BOARD_NFC_HAL_SUFFIX := msm8974
-
 # Samsung's nonstandard csd-client
 BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
-
-# QCOM support
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_QCOM_MEDIA_VARIANT := caf-new
-TARGET_QCOM_DISPLAY_VARIANT := caf-new
-BOARD_USES_LEGACY_ALSA_AUDIO := 
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_USES_QCOM_BSP := true
 
 # Audio settings
 BOARD_USES_CUSTOM_AUDIO_PLATFORM_PATH := device/samsung/lt03lte/audio/platform
@@ -97,11 +80,6 @@ AUDIO_FEATURE_DISABLED_ANC_HEADSET := true
 #AUDIO_FEATURE_DISABLED_INCALL_MUSIC := true
 #AUDIO_FEATURE_DISABLED_SPKR_PROTECTION := true
 #AUDIO_FEATURE_DISABLED_DS1_DOLBY_DDP := true
-
-WIFI_DRIVER_FW_PATH_P2P     := 
-
-# Don't use qcom camera HAL
-#USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Build lights 
 TARGET_PROVIDES_LIBLIGHT := true
@@ -127,14 +105,14 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 # Assert
 TARGET_OTA_ASSERT_DEVICE := lt03lte
 
-TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_lt03lte_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_defconfig
 
 # PowerHAL
 TARGET_POWERHAL_VARIANT := qcom
 TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/samsung/lt03lte/power/power_ext.c
 
-# The "new" GPS is really the old GPS, override it.
-BOARD_HAVE_NEW_QC_GPS :=
+# Consumerir
+TARGET_PROVIDES_CONSUMERIR_HAL := true
 
 # We don't use old-ass RPC
 TARGET_NO_RPC := true
