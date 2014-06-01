c481aa54b06f6ceed708cad49c309d789a366d8b
Schischu <schischu65@gmail.com>
2014-06-01 23:15:45 +0200

    Fix Keylayouts and bt

--- device.mk
+++ device.mk
@@ -79,10 +79,7 @@ PRODUCT_COPY_FILES += \
     device/samsung/lt03lte/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
     device/samsung/lt03lte/keylayout/Button_Jack.kl:system/usr/keylayout/Button_Jack.kl \
     device/samsung/lt03lte/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    device/samsung/lt03lte/keylayout/philips_remote_ir.kl:system/usr/keylayout/philips_remote_ir.kl \
-    device/samsung/lt03lte/keylayout/samsung_remote_ir.kl:system/usr/keylayout/samsung_remote_ir.kl \
-    device/samsung/lt03lte/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
-    device/samsung/lt03lte/keylayout/ue_rf4ce_remote.kl:system/usr/keylayout/ue_rf4ce_remote.kl
+    device/samsung/lt03lte/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
 
 # Ramdisk
 PRODUCT_PACKAGES += \
